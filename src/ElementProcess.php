53b559e5d4731ff955bdd4fc7d64de29a7a896e4
JaceRider <jacerider@gmail.com>
2025-05-19 12:38:39 -0500

    ✨ (ElementProcess.php): enhance tooltip functionality by adding support for custom placement and trigger options, improving user experience with better tooltip handling for buttons and descriptions.

--- src/ElementProcess.php
+++ src/ElementProcess.php
@@ -20,19 +20,45 @@ class ElementProcess {
     if (!empty($element['#neo_tooltip_built'])) {
       return $element;
     }
-    if (!empty($element['#description']) && (is_string($element['#description']) || $element['#description'] instanceof MarkupInterface)) {
-      if (isset($element['#tooltip']) && $element['#tooltip'] === FALSE) {
-        return $element;
-      }
+
+    // If tooltip is set and is FALSE, do not apply.
+    if (isset($element['#tooltip']) && $element['#tooltip'] === FALSE) {
+      return $element;
+    }
+
+    $tooltip = NULL;
+    $attributesProperty = 'attributes';
+    $options = [
+      'placement' => 'bottom-start',
+      'delay' => '[300,100]',
+      'triggerToNearestFocusableElement' => TRUE,
+    ];
+    if (isset($element['#type']) && in_array($element['#type'], ['button', 'submit'])) {
+      $options['placement'] = 'bottom';
+    }
+
+    // If we have a tooltip set as a string.
+    if (isset($element['#tooltip']) && (is_string($element['#tooltip']) || $element['#tooltip'] instanceof MarkupInterface)) {
+      $tooltip = new Tooltip($element['#tooltip'], $options);
+    }
+    // If we have a description set as a string.
+    elseif (!empty($element['#description']) && (is_string($element['#description']) || $element['#description'] instanceof MarkupInterface)) {
       $options = isset($element['#tooltip']) && is_array($element['#tooltip']) ? $element['#tooltip'] : [];
-      $options += [
+      $options = [
         'placement' => 'bottom-start',
         'delay' => '[300,100]',
         'triggerToNearestFocusableElement' => TRUE,
-      ];
+      ] + $options;
       $tooltip = new Tooltip($element['#description'], $options);
       $element['#description'] = NULL;
-      $tooltip->applyTo($element);
+    }
+
+    if ($tooltip) {
+      // Checkboxes and radios need to use the wrapper attributes.
+      if (isset($element['#type']) && in_array($element['#type'], ['checkboxes', 'radios'])) {
+        $attributesProperty = 'wrapper_attributes';
+      }
+      $tooltip->applyTo($element, '#' . $attributesProperty);
     }
     return $element;
   }
