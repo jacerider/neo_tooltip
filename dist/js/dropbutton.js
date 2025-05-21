2c0937a8ab0b606ea2720002db9992d25ffdc59b
JaceRider <jacerider@gmail.com>
2025-05-21 11:41:42 -0500

    Compiled file

--- dist/js/dropbutton.js
+++ dist/js/dropbutton.js
@@ -1,34 +1,34 @@
-(function(n, c, p) {
-  n.behaviors.neoTooltipDropbutton = {
-    attach: (b) => {
-      typeof c.neoTooltip > "u" || p("neo.tooltip.dropbutton", ".dropbutton", b).forEach((t) => {
-        var l;
+(function(i, c, b) {
+  i.behaviors.neoTooltipDropbutton = {
+    attach: (m) => {
+      typeof c.neoTooltip > "u" || b("neo.tooltip.dropbutton", ".dropbutton", m).forEach((t) => {
+        var l, u;
         const r = t.querySelector("li");
         if (!r) {
           console.error("No li elements found in the list");
           return;
         }
         t.classList.add("neo-dropbutton"), r.classList.add("dropbutton-action");
-        const d = document.createElement("ul"), i = t.querySelectorAll("li");
-        if (i.length <= 1) {
+        const d = document.createElement("ul"), n = t.querySelectorAll("li");
+        if (n.length <= 1) {
           t.classList.add("dropbutton-single");
           return;
         }
         t.classList.add("dropbutton-multiple");
-        for (let o = 1; o < i.length; o++) {
-          const u = i[o].cloneNode(!0);
-          (l = u.querySelector("a")) == null || l.removeAttribute("data-once"), d.appendChild(u), i[o].classList.add("sr-only");
+        for (let o = 1; o < n.length; o++) {
+          const p = n[o].cloneNode(!0);
+          (l = p.querySelector("a")) == null || l.removeAttribute("data-once"), d.appendChild(p), n[o].classList.add("sr-only"), (u = n[o].querySelector("a")) == null || u.removeAttribute("id");
         }
         const s = document.createElement("li");
         s.classList.add("dropbutton-toggle");
         const a = document.createElement("button");
-        a.setAttribute("type", "button"), a.innerHTML = '<span class="dropbutton-arrow"><span class="visually-hidden">' + n.t("List additional actions") + "</span></span>", s.appendChild(a), t.insertBefore(s, r.nextSibling);
+        a.setAttribute("type", "button"), a.innerHTML = '<span class="dropbutton-arrow"><span class="visually-hidden">' + i.t("List additional actions") + "</span></span>", s.appendChild(a), t.insertBefore(s, r.nextSibling);
         const e = {};
         e.content = d, e.theme = "neo dropbutton", e.trigger = "click", e.interactive = !0, e.placement = "bottom-end", e.onShow = (o) => {
-          n.attachBehaviors(o.popper, c), t.classList.add("is-active");
+          i.attachBehaviors(o.popper, c), t.classList.add("is-active");
         }, e.onHide = (o) => {
           t.classList.remove("is-active");
-        }, n.behaviors.neoTooltip.addInstance(s, e);
+        }, i.behaviors.neoTooltip.addInstance(s, e);
       });
     }
   };
