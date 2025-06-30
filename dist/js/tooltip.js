16162e0646eacff4de7203236d2c0f5ced890986
JaceRider <jacerider@gmail.com>
2025-06-30 14:25:49 -0500

    Compiled file

--- dist/js/tooltip.js
+++ dist/js/tooltip.js
@@ -1,56 +1,56 @@
 (function(s, o, l, p) {
-  const h = function(e) {
-    let t = document.querySelector(`link[neo-tooltip-animation-${e}]`);
-    t || (t = document.createElement("link"), t.rel = "stylesheet", t.type = "text/css", t.media = "screen", t.href = "/" + o.neoTooltip.dir + `/dist/css/tippy-${e}.css`, t.setAttribute(`neo-tooltip-animation-${e}`, ""), document.getElementsByTagName("head")[0].appendChild(t));
+  const h = function(t) {
+    let e = document.querySelector(`link[neo-tooltip-animation-${t}]`);
+    e || (e = document.createElement("link"), e.rel = "stylesheet", e.type = "text/css", e.media = "screen", e.href = "/" + o.neoTooltip.dir + `/dist/css/tippy-${t}.css`, e.setAttribute(`neo-tooltip-animation-${t}`, ""), document.getElementsByTagName("head")[0].appendChild(e));
   };
   s.behaviors.neoTooltip = {
     instances: [],
-    attach: function(e) {
-      this.hideAll(), !(typeof o.neoTooltip > "u") && l("neo-tooltip", ".use-neo-tooltip", e).forEach((t) => {
-        t.classList.contains("form-checkboxes") ? t.querySelectorAll(".form-type--checkbox").forEach((i) => {
+    attach: function(t) {
+      this.hideAll(), !(typeof o.neoTooltip > "u") && (t.classList.contains("use-neo-tooltip") && (t = t.parentElement || t), l("neo-tooltip", ".use-neo-tooltip", t).forEach((e) => {
+        e.classList.contains("form-checkboxes") ? e.querySelectorAll(".form-type--checkbox").forEach((i) => {
           this.addInstance(i);
-        }) : this.addInstance(t);
-      });
+        }) : this.addInstance(e);
+      }));
     },
-    addInstance: function(e, t) {
-      t = Object.assign({}, this.getOptions(e), t), this.instances.push(p(e, t));
+    addInstance: function(t, e) {
+      e = Object.assign({}, this.getOptions(t), e), this.instances.push(p(t, e));
     },
-    getOptions: (e) => {
-      const t = Object.assign({}, {
+    getOptions: (t) => {
+      const e = Object.assign({}, {
         theme: "neo",
         inertia: !0,
         maxWidth: 600
-      }, o.neoTooltip), i = e.getAttribute("data-tippy-animation") || t.animation;
-      if (i && ["shift-toward", "shift-away", "scale", "perspective"].includes(i) && h(i), e.getAttribute("data-tippy-trigger-nearest")) {
-        const n = e.closest("a, input, button");
-        if (n && n !== e)
-          t.triggerTarget = [n];
+      }, o.neoTooltip), i = t.getAttribute("data-tippy-animation") || e.animation;
+      if (i && ["shift-toward", "shift-away", "scale", "perspective"].includes(i) && h(i), t.getAttribute("data-tippy-trigger-nearest")) {
+        const n = t.closest("a, input, button");
+        if (n && n !== t)
+          e.triggerTarget = [n];
         else {
-          const c = e.closest("label");
+          const c = t.closest("label");
           if (c)
-            t.triggerTarget = [c];
+            e.triggerTarget = [c];
           else {
-            const r = e.closest("input");
-            r && (t.triggerTarget = [r]);
+            const r = t.closest("input");
+            r && (e.triggerTarget = [r]);
           }
         }
       }
-      const a = e.getAttribute("data-tippy-template");
-      return a && o.neoTooltipTemplates && o.neoTooltipTemplates[a] && (t.allowHTML = !0, t.interactive = !0, t.content = o.neoTooltipTemplates[a]), t.onShow = (n) => n.props.content.length != 0, t;
+      const a = t.getAttribute("data-tippy-template");
+      return a && o.neoTooltipTemplates && o.neoTooltipTemplates[a] && (e.allowHTML = !0, e.interactive = !0, e.content = o.neoTooltipTemplates[a]), e.onShow = (n) => n.props.content.length != 0, e;
     },
     hideAll: () => {
-      s.behaviors.neoTooltip.instances.forEach((e) => {
-        e.hide();
+      s.behaviors.neoTooltip.instances.forEach((t) => {
+        t.hide();
       });
     },
     disableAll: () => {
-      s.behaviors.neoTooltip.instances.forEach((e) => {
-        e.disable();
+      s.behaviors.neoTooltip.instances.forEach((t) => {
+        t.disable();
       });
     },
     enableAll: () => {
-      s.behaviors.neoTooltip.instances.forEach((e) => {
-        e.enable();
+      s.behaviors.neoTooltip.instances.forEach((t) => {
+        t.enable();
       });
     }
   };
